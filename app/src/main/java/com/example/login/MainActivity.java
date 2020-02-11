0b9aec41bfce70b254d394b70b754ee1abac6f39
afroz23 <afroziiitian@gmail.com>
2020-02-11 22:26:09 +0530

    constraint full added

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoveToActivityTwo();
+            }
+        });
+    }
+    private void MoveToActivityTwo()
+    {
+        Intent intent = new Intent(MainActivity.this,second_activity.class);
+        startActivity(intent);
     }
 }
